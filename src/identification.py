8e54ee9ef89ef7d23a961deab00603d6a1457a77
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2022-11-22 01:15:25 +0700

    Identification func

--- src/identification.py
+++ src/identification.py
@@ -1,5 +1,26 @@
 import numpy
-import os
+from mean import *
 
-def getNewEigenface():
-    return 0
+def getNewEigenface(image_input, image_path, eigenvector):
+    selisih = image_input - mean(image_path)
+    testface = numpy.dot(selisih, eigenvector)
+    min = 999
+    name = None
+
+    database_eigenface_path = 'src/Database/databaseEigen.pck'
+
+    dbfile = open(database_eigenface_path, 'rb')
+    db = pickle.load(dbfile)
+    for value in db:
+        hasil = np.subtract(testface, db[value])
+        sum = 0
+        for i in range(len(hasil)):
+            sum += numpy.power(hasil[i], 2)
+        distance = numpy.sqrt(sum)
+        if distance < min:
+            min = distance
+            name = value
+            
+    dbfile.close()
+
+    return name, min
